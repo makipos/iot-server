858e98ae8545443fff40cefa4a414831f024735a
tranphuong.080 <tranphuong.080@gmail.com>
2019-05-16 17:51:51 +0700

    add install docker compose

--- script/prepairDocker.sh
+++ script/prepairDocker.sh
@@ -4,11 +4,14 @@ if [ (count $argv) = 0 ]
   #statements
   echo "You need pass ip interface in argument"
 else
-  apt-get update
+  # apt-get update
   apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
   curl -fsSL https://get.docker.com -o get-docker.sh
   sh get-docker.sh
 
   docker swarm init --advertise-addr $argv
+
+  curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-(uname -s)-(uname -m)" -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
 end
