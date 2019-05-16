aec74c6296027a9e20d4dbf899754c0d1ae62931
tranphuong.080 <tranphuong.080@gmail.com>
2019-05-16 17:47:00 +0700

    need pass ip of network interface

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/fish
 
-./script/prepairDocker.sh
-./script/prepair.sh
+if [ (count $argv) = 0 ]
+  #statements
+  echo "You need pass ip interface in argument"
+else
+  ./script/prepairDocker.sh $argv
+  ./script/prepair.sh
 
-./scriptdeploy/deploy.sh
-./script/prepairdb.sh
+  ./scriptdeploy/deploy.sh
+  ./script/prepairdb.sh
+end
--- script/prepairDocker.sh
+++ script/prepairDocker.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
+#!/usr/bin/fish
 
-apt-get update
-apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+if [ (count $argv) = 0 ]
+  #statements
+  echo "You need pass ip interface in argument"
+else
+  apt-get update
+  apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sh get-docker.sh
 
-docker swarm init
+  docker swarm init --advertise-addr $argv
+end
