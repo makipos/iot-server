01b45325d02dc3b6e42cbe43a64342293aed5345
tranphuong.080 <tranphuong.080@gmail.com>
2019-05-16 18:08:09 +0700

    update

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ if [ (count $argv) = 0 ]
   #statements
   echo "You need pass ip interface in argument"
 else
+  apt-get update
+  apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common sshpass
   ./script/prepairDocker.sh $argv
   ./script/prepair.sh
 
--- script/prepairDocker.sh
+++ script/prepairDocker.sh
@@ -3,10 +3,7 @@
 if [ (count $argv) = 0 ]
   #statements
   echo "You need pass ip interface in argument"
-else
-  # apt-get update
-  apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-
+else    
   curl -fsSL https://get.docker.com -o get-docker.sh
   sh get-docker.sh
 
