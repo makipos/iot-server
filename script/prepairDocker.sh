dbfba027d2d66c20a77ec11adabaa53623fb9771
tranphuong.080 <tranphuong.080@gmail.com>
2019-05-16 17:39:02 +0700

    fix install docker script

--- script/prepairDocker.sh
+++ script/prepairDocker.sh
@@ -3,10 +3,7 @@
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-apt-key fingerprint 0EBFCD88
-
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
 
 docker swarm init
