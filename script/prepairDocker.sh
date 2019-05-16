d08acf13da1791934eb6555904e550f085194bac
tranphuong.080 <tranphuong.080@gmail.com>
2019-05-16 17:33:50 +0700

    add -y

--- script/prepairDocker.sh
+++ script/prepairDocker.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 apt-get update
-apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
 
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce docker-ce-cli containerd.io
 
 docker swarm init
