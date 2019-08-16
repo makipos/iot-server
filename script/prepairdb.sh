3bec340c9e1eeb60f34a842e5a1635a238c01c46
tranphuong.080 <tranphuong.080@gmail.com>
2019-08-16 10:04:22 +0700

    fix prepairdb

--- script/prepairdb.sh
+++ script/prepairdb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sshpass -p MKP123456a@ ssh -o "StrictHostKeyChecking no" root@localhost -p 2222 "mongo --host mongodb << $(cat script/mongoshellcommand.js)"
+sshpass -p MKP123456a@ ssh -o "StrictHostKeyChecking no" root@127.0.0.1 -p 2222 "mongo --host mongodb << $(cat script/mongoshellcommand.js)"
