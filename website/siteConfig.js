2436988043974abe74ed0e30e58e69c4afbeb094
tranphuong.080 <tranphuong.080@gmail.com>
2020-01-16 17:06:30 +0700

    .

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Makipos', // Title for your website.
   tagline: 'Nền tảng iot Makipos',
   url: 'https://makipos.github.io', // Your website URL
-  baseUrl: '/iot-server', // Base URL for your project */
+  baseUrl: '/iot-server/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
