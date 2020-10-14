dabb3a164f135c473cda5809e4722cd0c059ea98
tranphuong.080 <tranphuong.080@gmail.com>
2020-10-14 15:11:25 +0700

    add files service

--- script/prepair.sh
+++ script/prepair.sh
@@ -7,12 +7,13 @@ mkdir -p /media/Programs/Programs_data/makiposiot/redis
 mkdir -p /media/Programs/Programs_data/makiposiot/redis_cache
 
 mkdir -p /media/Programs/Programs_data/makiposiot/emqtt/certs
+
 mkdir -p /media/Programs/Programs_data/makiposiot/update_service/logs
 mkdir -p /media/Programs/Programs_data/makiposiot/update_service/release
 mkdir -p /media/Programs/Programs_data/makiposiot/update_service/release-device
 mkdir -p /media/Programs/Programs_data/makiposiot/devices_service/logs
-mkdir -p /media/Programs/Programs_data/makiposiot/devices_service/image-device
 mkdir -p /media/Programs/Programs_data/makiposiot/automatic_service/logs
 mkdir -p /media/Programs/Programs_data/makiposiot/relations_service/logs
 mkdir -p /media/Programs/Programs_data/makiposiot/users_service/logs
-mkdir -p /media/Programs/Programs_data/makiposiot/users_service/image-icon
+mkdir -p /media/Programs/Programs_data/makiposiot/files_service/logs
+mkdir -p /media/Programs/Programs_data/makiposiot/files_service/files
