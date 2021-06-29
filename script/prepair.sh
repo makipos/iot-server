d5949d26fed2f97fc278103b0cc8b22937440630
tranphuong.080 <tranphuong.080@gmail.com>
2021-06-29 16:03:24 +0700

    update use only one traefik

--- script/prepair.sh
+++ script/prepair.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-chmod 600 config/traefik-frontend/acme/acme.json
+chmod 600 config/traefik/acme/acme.json
 
 mkdir -p /media/Programs/Programs_data/makiposiot/mongodb
 mkdir -p /media/Programs/Programs_data/makiposiot/redis
 mkdir -p /media/Programs/Programs_data/makiposiot/redis_cache
-
-mkdir -p /media/Programs/Programs_data/makiposiot/emqtt/certs
 
 mkdir -p /media/Programs/Programs_data/makiposiot/update_service/logs
 mkdir -p /media/Programs/Programs_data/makiposiot/update_service/release
--- scriptdeploy/createNetwork.sh
+++ scriptdeploy/createNetwork.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/fish
 
 docker network create --opt encrypted --attachable --driver=overlay --subnet=10.200.0.0/16 makiposiot-internal
+docker network create --opt encrypted --attachable --driver=overlay --subnet=10.201.0.0/16 makiposiot-frontend
