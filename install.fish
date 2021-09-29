54fbd57adff4640e54a8f8ae0c67b086ce70c7de
tranphuong.080 <tranphuong.080@gmail.com>
2021-09-29 15:10:24 +0700

    ...

--- install.fish
+++ install.fish
@@ -2,7 +2,7 @@
 
 if [ (count $argv) = 0 ]
   #statements
-  echo "You need pass ip interface in argument"
+  echo "You need pass ip interface in argument to init docker swarm"
 else
   apt-get update
   apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common sshpass fish sh bash
